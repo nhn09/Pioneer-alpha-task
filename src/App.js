41f358356aea4da34fd497e5a4318e2f92121b17
nhn09 <68825961+nhn09@users.noreply.github.com>
2021-03-24 13:32:47 +0600

    axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Header from './components/UI/header';
 
 const App=() => {
+  const[items,setItems] = useState ([])
+  const[isLoading,setIsLoading] = useState ([true])
+
+
+ useEffect(()=>
+ {
+   const fetchItems = async () => 
+   {
+     const result = await axios (`https://www.breakingbadapi.com/api/characters`)
+
+     console.log(result.data)
+   }
+
+   fetchItems()
+ },[])
+
+
   return (
     <div className="Container">
+     <Header/>
      Starting
     </div>
   );
---
+++ src/components/UI/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../../src/App.css'
+
+const Header = () => {
+    return (
+        <header className='center-header'>
+            <div>
+                I am navbar
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header
+
