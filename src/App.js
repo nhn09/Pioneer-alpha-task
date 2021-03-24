e00e7c4caeae6b29943450f5f44f25ab85d40300
nhn09 <68825961+nhn09@users.noreply.github.com>
2021-03-24 11:58:15 +0600

    cleaning cont

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App=() => {
   return (
-    <div className="App">
+    <div className="Container">
      
     </div>
   );
